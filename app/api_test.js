087ee64d16b7841300ddc7fd78f8dc1dba2f67fc
MTonne <bastianschmeier@gmail.com>
2017-02-05 13:41:20 +0100

    add initial ugly code (don't judge me...)
    * use YT API test with ability to read comments and upload videos

---
+++ app/api_test.js
@@ -0,0 +1,155 @@
+/**
+ * This script uploads a video (specifically `video.mp4` from the current
+ * directory) to YouTube,
+ *
+ * To run this script you have to create OAuth2 credentials and download them
+ * as JSON and replace the `credentials.json` file. Then install the
+ * dependencies:
+ *
+ * npm i r-json lien opn bug-killer
+ *
+ * Don't forget to run an `npm i` to install the `youtube-api` dependencies.
+ * */
+ 
+const Youtube = require("youtube-api")
+    , fs = require("fs")
+    , readJson = require("r-json")
+    , Lien = require("lien")
+    , Logger = require("bug-killer")
+    , opn = require("opn")
+    , prettyBytes = require("pretty-bytes")
+    ;
+ 
+var uploadTest = function ()
+{
+	var req = Youtube.videos.insert(
+	{
+		resource:
+		{
+			// Video title and description 
+			snippet:
+			{
+				title: "Testing YoutTube API NodeJS module"
+				, description: "Test video upload via YouTube API"
+			}
+			// I don't want to spam my subscribers 
+			, status:
+			{
+				privacyStatus: "private"
+			}
+		}
+		// This is for the callback function 
+		, part: 'snippet,status'
+
+		// Create the readable stream to upload the video 
+		, media:
+		{
+			body: fs.createReadStream('video.mp4')
+		}
+	}
+	, (err, data) =>
+	{
+		console.log("Done.");
+		process.exit();
+	});
+
+	setInterval(function ()
+	{
+		Logger.log(`${prettyBytes(req.req.connection._bytesDispatched)} bytes uploaded.`);
+	}, 250);
+}
+
+
+var displayComments = function (data)
+{
+	for (let comment of data.items)
+	{
+		console.log(comment.snippet.topLevelComment.snippet.textDisplay);
+	}
+}
+
+
+var commentsTest = function ()
+{
+	var req = Youtube.commentThreads.list(
+	{
+		
+		// video id for comments
+		videoId: 'oXKSkdHVUzs'
+
+	
+		// This is for the callback function 
+		, part: 'snippet'
+
+	}
+	, (err, data) =>
+	{
+		if (data) 
+		{
+			// console.log('data: ', data);
+			displayComments(data);
+		}
+		else
+		{
+			console.log('err: ', err);
+		}
+		console.log("Done.");
+		process.exit();
+	});
+}
+
+
+
+// I downloaded the file from OAuth2 -> Download JSON 
+const CREDENTIALS = readJson(`./credentials.json`);
+ 
+// Init lien server 
+let server = new Lien({
+    host: "localhost"
+  , port: 5000
+});
+ 
+// Authenticate 
+// You can access the Youtube resources via OAuth2 only. 
+// https://developers.google.com/youtube/v3/guides/moving_to_oauth#service_accounts 
+let oauth = Youtube.authenticate({
+    type: "oauth"
+  , client_id: CREDENTIALS.web.client_id
+  , client_secret: CREDENTIALS.web.client_secret
+  , redirect_url: CREDENTIALS.web.redirect_uris[0]
+});
+ 
+opn(oauth.generateAuthUrl({
+	access_type: "offline"
+	, scope: [
+		'https://www.googleapis.com/auth/youtube.force-ssl'
+		// 'https://www.googleapis.com/auth/youtube'
+		// , 'https://www.googleapis.com/auth/youtube.upload'
+
+	]
+}));
+
+
+ 
+// Handle oauth2 callback 
+server.addPage("/oauth2callback", lien =>
+{
+	Logger.log("Trying to get the token using the following code: " + lien.query.code);
+	oauth.getToken(lien.query.code, (err, tokens) =>
+	{
+ 
+        if (err) {
+            lien.lien(err, 400);
+            return Logger.log(err);
+        }
+ 
+        Logger.log("Got the tokens.");
+ 
+        oauth.setCredentials(tokens);
+ 
+        lien.end('Token is ' + JSON.stringify(tokens) + '. Credentials set. Lien-Server closed.');
+ 
+		// uploadTest();
+		commentsTest();
+    });
+});
